09214d85fe9010b52d867e76ef02f55a3ac759f2
Miriana Calvano <62550924+mcalvano5@users.noreply.github.com>
2022-08-24 17:30:02 +0200

    Create grammar.hs

---
+++ grammar.hs
@@ -0,0 +1,42 @@
+program ::= <command> | <command> <program>
+
+command ::= <assignmentArray> 
+            | <assignmentArith> 
+            | <assignmentBool> 
+            | <skip> 
+            | <ifThenElse> 
+            | <while> 
+            | <declareArith> 
+            | <declareBool> 
+            | <declareArray>
+            | <declareChar>
+            | <declareDouble>
+
+assignmentArray ::=    
+
+assignmentArith ::=
+
+assignmentBool ::=
+
+skip ::=
+
+ifThenElse ::=
+
+while ::=
+
+declareArith ::=
+
+declareBool ::=
+
+declareArray ::=
+
+declareChar ::= fare lower e upper
+
+declareDouble ::= usare punto '.' tra le digit
+
+digit ::= 
+
+arithExp ::= 
+boolExp :==
+
+
