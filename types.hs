a62da3a8e5992d415075154f8d177d01a864a9e3
Miriana Calvano <62550924+mcalvano5@users.noreply.github.com>
2022-08-25 12:20:27 +0200

    modifica float

---
+++ types.hs
@@ -0,0 +1,11 @@
+
+data Type = IntType Int
+            | FloatType Float
+            | CharType Char
+            | BoolType Bool
+            | ArrayTypeInt [Int]
+            | ArrayTypeChar [Char]
+            | ArrayTypeFloat [Float]
+            deriving Show
+
+            
